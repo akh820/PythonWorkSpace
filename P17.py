de10e21edbe5b9e98645298f49ac9fff9ecea62f
akh820 <akh8202@gmail.com>
2023-08-21 15:44:39 +0900

    Function
    Function

---
+++ P17.py
@@ -0,0 +1,15 @@
+# 함수 2
+
+def someFunc(x1, x2, x3):
+    result = x1 + x2
+    result = result * x3
+    # print(result)
+    return result # 함수는 return문을 만나면 탈출한다.
+                  # result를 되돌려주겠다.
+
+print("코드...")
+print("코드...")
+print("코드...")
+
+qwer = someFunc(1, 2, 3)
+print(qwer)
