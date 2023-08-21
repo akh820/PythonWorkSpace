150a209082b5f79b0463b940889f28d4118e1606
akh820 <akh8202@gmail.com>
2023-08-21 11:29:55 +0900

    Update Q8.py
    소수

--- Q8.py
+++ Q8.py
@@ -2,7 +2,7 @@
 value = 863
 a = 2
 isPrime = True # 한번이라도 걸리면 False로 스위칭?
-while a < value: 
+while a < value/2: # 2로 나누는 이유는 속도를 빠르게 어차피 몫이 1이상이나옴 
 
     if value % a==0:
         isPrime = False
