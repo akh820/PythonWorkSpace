14ba38c09966c18ecf75ed5305ad2824c82136c8
akh820 <akh8202@gmail.com>
2023-08-21 13:39:56 +0900

    Update Q8.py
    소수 method

--- Q8.py
+++ Q8.py
@@ -1,10 +1,10 @@
 # 863은 소수인가?
-value = 863
+value = 400
 a = 2
 isPrime = True # 한번이라도 걸리면 False로 스위칭?
-while a < value/2: # 2로 나누는 이유는 속도를 빠르게 어차피 몫이 1이상이나옴 
+while a < value: # 2로 나누는 이유는 속도를 빠르게 어차피 몫이 1이상이나옴 
 
-    if value % a==0:
+    if value % a == 0: #소수를 판별하는 
         isPrime = False
         break
 
